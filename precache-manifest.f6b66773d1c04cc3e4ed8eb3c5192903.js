e3e9484e363a0b2c59f03f6066eea01864756865
Gian <gap.tobias@gmail.com>
2020-05-05 14:35:50 +0800

    Updates

---
+++ precache-manifest.f6b66773d1c04cc3e4ed8eb3c5192903.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a0000c592c1e85eef384448986f4bad",
+    "url": "/slurm-job-generator/index.html"
+  },
+  {
+    "revision": "07b36c60dc8bdaea5339",
+    "url": "/slurm-job-generator/static/css/main.8cde849e.chunk.css"
+  },
+  {
+    "revision": "04aec8ed14a93a564915",
+    "url": "/slurm-job-generator/static/js/2.258fc062.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/slurm-job-generator/static/js/2.258fc062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07b36c60dc8bdaea5339",
+    "url": "/slurm-job-generator/static/js/main.ef288f93.chunk.js"
+  },
+  {
+    "revision": "2fe3e7fdc20b6ea55468",
+    "url": "/slurm-job-generator/static/js/runtime-main.5d957f44.js"
+  }
+]);
